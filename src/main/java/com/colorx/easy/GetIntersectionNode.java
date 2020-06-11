5b06f9f35a9a5d8ec98ea69a00b7cdd9d5e6ee93
欧景斌 <1940776098@qq.com>
2020-06-11 12:00:11 +0800

    找出两个链表的交点

--- src/main/java/com/colorx/easy/GetIntersectionNode.java
+++ src/main/java/com/colorx/easy/GetIntersectionNode.java
@@ -4,6 +4,18 @@ package com.colorx.easy;
  * @author colorx
  * @version 1.0
  * @date 2020/6/11 11:17
+ * A:          a1 → a2       d1 → d2
+ *                     ↘  ↗
+ *                       c
+ *                     ↗  ↘
+ * B:    b1 → b2 → b3        e1 → e2
+ * 要求时间复杂度为 O(N)，空间复杂度为 O(1)。如果不存在交点则返回 null。
+ *
+ * 设 A 的长度为 a + c，B 的长度为 b + c，其中 c 为尾部公共部分长度，可知 a + c + b = b + c + a。
+ *
+ * 当访问 A 链表的指针访问到链表尾部时，令它从链表 B 的头部开始访问链表 B；同样地，当访问 B 链表的指针访问到链表尾部时，令它从链表 A 的头部开始访问链表 A。这样就能控制访问 A 和 B 两个链表的指针能同时访问到交点。
+ *
+ * 如果不存在交点，那么 a + b = b + a，以下实现代码中 l1 和 l2 会同时为 null，从而退出循环。
  */
 public class GetIntersectionNode {
 
